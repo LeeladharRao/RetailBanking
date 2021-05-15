002b4ac2e14ae96bf53e327543d8a5f7ff3da76d
LeeladharRao <kompallyleeladhar@gmail.com>
2021-05-15 18:04:44 +0530

    Added resgistration of users

--- retial_banking/routes.py
+++ retial_banking/routes.py
@@ -54,3 +54,160 @@ def login():
             # end setup
 
             return redirect(url_for('home'))
+
+
+@app.route('/registerExecutive', methods=['get', 'post'])
+def registerExecutive():
+
+    if request.method == "GET":
+        autodata = {}
+        sid = edb.getautoSSNid()
+        flash("Auto generated SSN ID : "+sid)
+        autodata['ssn_id'] = sid
+        return render_template('registerExecutive.html', registerExecutive=True, autodata=autodata)
+
+    regdata = {}
+
+    regdata['ssn_id'] = request.form.get('ssn')
+    if len(regdata['ssn_id'])<9:
+        flash("SSN ID length should be of minimum 9 ","danger")
+        return redirect(url_for('registerExecutive'))
+    if not str(regdata['ssn_id']).isdecimal():
+        flash("SSN ID should only be numerical ","danger")
+        return redirect(url_for('registerExecutive'))
+
+
+    regdata['name'] =str( request.form.get('name')).replace("\n","")
+    regdata['email'] = request.form.get('email')
+    regdata['pass'] = hashlib.sha256(request.form.get('psw').encode()).hexdigest()
+
+    if len(regdata['name'])<3:
+        flash("Name Should be of minimum 3 Characters ","danger")
+        return redirect(url_for('registerExecutive'))
+
+    if not utility.isNameValid(regdata['name']):
+        flash("Entered Name is invalid ","danger")
+        return redirect(url_for('registerExecutive'))
+
+    if len(regdata['email'])<1 and not '@' in regdata['email']:
+        flash("Invalid Email entered ","danger")
+        return redirect(url_for('registerExecutive'))
+
+    if len(regdata['pass'])<1:
+        flash("minimum length of password must be 1 character ","danger")
+        return redirect(url_for('registerExecutive'))
+
+
+    regdata['creation_time'] =utility.getTime()
+    regdata['access_ip']=request.headers.get("x-forwarded-for",request.remote_addr)
+
+    result, err = edb.register(regdata)
+
+    if result:
+        flash("Executive Registered Successfully ...    Login Now", "success")
+
+        ###EMAIL SEND##
+        data={}
+        data['type']=utility.EMAIL_REG_EXECUTIVE
+        data['ssn_id']=regdata['ssn_id']
+        data['name']=regdata['name']
+        data['to']=regdata['email']
+        utility.sendEmail(data)
+        #####
+        return redirect(url_for('login'))
+    else:
+        flash("Failed to Register :"+err, "danger")
+        return redirect(url_for('registerExecutive'))
+
+    return redirect('login.html')
+
+
+@app.route('/registerCustomer', methods=['get', 'post'])
+def registerCustomer():
+
+    if not isLoggedin():
+        # if there is no one loggedIn disallow this route
+        flash("Login first to access it ", "danger")
+        return redirect(url_for('home'))
+
+    if request.method == "GET":
+        autodata = {}
+        sid = cdb.getautoSSNid()
+        flash("Auto generated SSN ID : "+sid)
+        autodata['ssn_id'] = sid
+        autodata['states']=utility.getState()
+        return render_template('registerCustomer.html', registerCustomer=True, autodata=autodata)
+
+    regdata = {}
+
+    regdata['ssn_id'] = request.form.get('ssn')
+    if len(regdata['ssn_id'])<9:
+        flash("SSN ID length should be of minimum 9 ","danger")
+        return redirect(url_for('registerCustomer'))
+    if not str(regdata['ssn_id']).isdecimal():
+        flash("SSN ID should only be numerical ","danger")
+        return redirect(url_for('registerCustomer'))
+
+    regdata['name'] = str(request.form.get('name')).replace("\n","").replace("  "," ")
+    regdata['age'] = request.form.get('age')
+    regdata['state'] = request.form.get('state')
+    regdata['address'] = request.form.get('address')
+    regdata['email']=request.form.get('cust_email')
+    regdata['create_time']=utility.getTime()
+    regdata['access_ip']=request.headers.get("x-forwarded-for",request.remote_addr)
+
+
+    if len(regdata['name'])<3:
+        flash("Name Should be of minimum 3 Characters ","danger")
+        return redirect(url_for('registerCustomer'))
+    if not utility.isNameValid(regdata['name']):
+        flash("Entered Name is not Valid ","danger")
+        return redirect(url_for('registerCustomer'))
+    if not utility.isNameValid(regdata['address']):
+        flash("Entered Address is not Valid ","danger")
+        return redirect(url_for('registerCustomer'))
+
+    if not utility.isNameValid(regdata['name']):
+        flash(f"Entered Name={regdata['name']} is not Valid  ","danger")
+        return redirect(url_for('registerCustomer'))
+
+    
+    if len(regdata['address'])<3:
+        flash("Address Should be of minimum 3 Characters ","danger")
+        return redirect(url_for('registerCustomer'))
+
+    try:
+        if int(regdata['age'])<18:
+            flash("Customer should be of minimum 18 years old to Register ","danger")
+            return redirect(url_for('registerCustomer'))
+    except :
+            flash("Customer should be of minimum 18 years old to Register ","danger")
+            return redirect(url_for('registerCustomer'))
+
+    if not utility.isStateValid(regdata['state']):
+        flash("Select State from dropdown correctly ","danger")
+        return redirect(url_for('registerCustomer'))
+        
+
+    result, err = cdb.registerSSN(regdata)
+
+    if result:
+        flash("Customer Registered Successfully", "success")
+                ###EMAIL SEND##
+        data={}
+        data['type']=utility.EMAIL_REG_CUSTOMER
+        data['ssn_id']=regdata['ssn_id']
+        data['name']=regdata['name']
+        data['to']=regdata.get('email',None)
+        if data['to'] !=None:
+            utility.sendEmail(data)
+        else:
+            logging.error(f"No valid Email found for {data['ssn_id']}")
+        #####
+        return redirect(url_for('viewCustomerDetail')+"/"+regdata['ssn_id'])
+    else:
+        flash("Failed to Register Customer "+err, "danger")
+
+    return render_template('registerCustomer.html', registerCustomer=True)
+
+
